dc054e72513a7bf612170858ae5d3e87ab2cf348
lilyyen <lilyyen@kkbox.com>
2013-06-24 13:41:32 +0800

    新增
    aaa

---
+++ 9x9_base3.php
@@ -0,0 +1,19 @@
+<?
+  // this is a test!
+  $i = 1;
+  while($i < 10) {
+    if(++$j < 10) {
+      echo $i."x".$j." = ". echo ($i*$j);
+    } else {
+      $i++;
+      $j = 1;
+    }
+  }
+
+  echo "\n\n\n";
+
+  $j = 1;
+  for($i=1; $i<10; ($j++<9)?$i:($j=($i++)*0+1)) {
+      echo $i."x".$j." = ".sprintf("%4d\n", ($i*$j));
+  }
+?>
